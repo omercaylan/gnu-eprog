2fd5ed83869d96901d3380aa8af46a81b6bb5c57
omercaylan <omerceylan38@gmail.com>
2020-01-21 23:00:18 +0300

    makefile fix->clean and map file added, linker and c code changes NOT tested.

--- RamLoader/csum.c
+++ RamLoader/csum.c
@@ -1,3 +1,14 @@
+#define MAX_DATA_SIZE 10
+struct{
+        int Data[MAX_DATA_SIZE];
+}sSensorData;
+
+
+extern unsigned long _SensorData;
+extern unsigned long _SensorDataLen;
+struct sSensorData *gSensor = (struct sSensorData *) &_SensorData;
+
+
 static int arr[] = { 1, 10, 4, 5, 6, 7 };
 static int sum;
 static const int n = sizeof(arr) / sizeof(arr[0]);
